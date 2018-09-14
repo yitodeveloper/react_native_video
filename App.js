25b971282fb27992ce2cc1f7bd210a712716341e
Rodrigo Ramírez <yitodeveloper@gmail.com>
2018-09-14 16:15:10 -0300

    llamado a api de videos y carga de data a lista horizontal

--- App.js
+++ App.js
@@ -3,9 +3,19 @@ import {Text} from 'react-native';
 import Home from './src/screens/containers/home'
 import Header from './src/sections/components/header'
 import SuggestionList from './src/videos/containers/suggestion-list'
-
+import Api from './utils/api'
 
 export default class App extends Component {
+  state = {
+    suggestionList: []
+  }
+  async componentDidMount(){
+    const movies =  await Api.getSuggestion(434)
+    console.log(movies)
+    this.setState({
+      suggestionList: movies
+    })
+  }
   render() {
     return (
       <Home>
@@ -14,7 +24,9 @@ export default class App extends Component {
           </Header>
           <Text>Buscador</Text>
           <Text>Categorias</Text>
-          <SuggestionList/>
+          <SuggestionList
+            list={this.state.suggestionList}
+          />
       </Home>
     );
   }
--- src/videos/containers/suggestion-list.js
+++ src/videos/containers/suggestion-list.js
@@ -3,45 +3,51 @@ import {FlatList, Text} from 'react-native'
 import Layout from '../components/suggestion-list-layout'
 import Empty from '../components/empty'
 import Separator from '../components/vertical-separator'
+import Suggestion from '../components/suggestion'
 
 class SuggestionList extends Component{
     renderEmpty = () => <Empty text="No hay sugerencias :(" />
     itemSeparator = () => <Separator />
+    renderItem = ({item}) => (
+        <Suggestion {...item} />
+    )
+    keyExtractor = (item) => (
+        item.id.toString()
+    )
 
     render(){
-        const list = [
-            {
-                key: "1",
-                title: "Inside Out"
-            },
-            {
-                key: "2",
-                title: "StarWars 7: The force Awakens"
-            },
-            {
-                key: "3",
-                title: "Tangled"
-            },
-            {
-                key: "4",
-                title: "Titanic"
-            },
-            {
-                key: "5",
-                title: "Wishbone"
-            }
-        ]
+        // const list = [
+        //     {
+        //         key: "1",
+        //         title: "Inside Out"
+        //     },
+        //     {
+        //         key: "2",
+        //         title: "StarWars 7: The force Awakens"
+        //     },
+        //     {
+        //         key: "3",
+        //         title: "Tangled"
+        //     },
+        //     {
+        //         key: "4",
+        //         title: "Titanic"
+        //     },
+        //     {
+        //         key: "5",
+        //         title: "Wishbone"
+        //     }
+        // ]
         return(
             <Layout
                 title="Recomendado para ti"
             >
                 <FlatList
-                    data={list}
+                    keyExtractor={this.keyExtractor}
+                    data={this.props.list}
                     ListEmptyComponent={this.renderEmpty}
                     ItemSeparatorComponent={this.itemSeparator}
-                    renderItem={({item})=>(
-                        <Text>{item.title}</Text>
-                    )}
+                    renderItem={this.renderItem}
                 />
             </Layout>
             
